81733c6598df8d8d71700ea1a8c3f0fd3a47bef1
benazirshaik11 <benazirbanushaik@gmail.com>
2024-06-15 17:50:20 +0100

    refactoring

--- src/main/java/com/skenariolabs/propertyviewer/config/MvcConfig.java
+++ src/main/java/com/skenariolabs/propertyviewer/config/MvcConfig.java
@@ -9,8 +9,6 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/").setViewName("login");
-        registry.addViewController("/logout").setViewName("login");
-
+        registry.addViewController("/").setViewName("redirect:/buildings");
     }
 }
--- src/main/java/com/skenariolabs/propertyviewer/config/WebSecurityConfig.java
+++ src/main/java/com/skenariolabs/propertyviewer/config/WebSecurityConfig.java
@@ -1,14 +1,10 @@
 package com.skenariolabs.propertyviewer.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,29 +16,20 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/css/style.css", "/buildings").permitAll()
-                        .anyRequest().authenticated()
-                )
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/", "/buildings").permitAll()
+                        .anyRequest().authenticated())
                 .formLogin((form) -> form
-                        .loginPage("/login").defaultSuccessUrl("/buildings")
-                        .permitAll()
-                )
-                .logout(LogoutConfigurer::permitAll)
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/buildings")
+                        .permitAll())
+                .logout(logoutConfigurer -> logoutConfigurer
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/buildings")
+                        .permitAll())
                 .httpBasic(withDefaults())
                 .csrf().disable();
 
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("pwd")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
     }
 }
